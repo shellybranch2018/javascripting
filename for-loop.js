fd7a2a08f2da7e7d0633e4d4b59af5fdf10f7a49
shellybranch2018 <sbranch@homeaway.com>
2019-01-03 21:00:52 +0000

    Inital commit with all my work so far.

---
+++ for-loop.js
@@ -0,0 +1,3 @@
+var total = 0; var limit = 10; for(var i = 0; i < limit; i++ ){
+  console.log(total += i);
+}
---
+++ looping-through-arrays.js
@@ -0,0 +1,3 @@
+var pets = ['cat', 'dog', 'rat']; for( i = 0; i < pets.length; i++){
+  console.log(pets[i] = pets[i] + 's')
+}
